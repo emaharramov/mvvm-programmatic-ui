ff2d7c12e154e2892fd61206cc1120db22a46032
emaharramov <emil.mhr@div.edu.az>
2024-11-11 09:58:35 +0400

    Initial Commit

---
+++ mvvm-programmatic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mvvm-programmatic
+//
+//  Created by Emil Maharramov on 11.11.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
